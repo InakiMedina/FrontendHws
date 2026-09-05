628bb65c41095bc6b0d97e665a2666244285953d
InakiMedina <inaki.medina@gmail.com>
2026-09-04 21:08:31 -0600

    Improved hero banner

--- tarea1/my-app/src/Content/HeroBanner.tsx
+++ tarea1/my-app/src/Content/HeroBanner.tsx
@@ -1,8 +1,10 @@
 import styles from "./HeroBanner.module.css"
+import spooderman from '../Assets/spooderman.gif'
 
 export default function HeroBanner() {
 	return (
 		<div className={styles.banner}>
+			<img className={styles.img} src ={spooderman}/>
 			<h1>Iñaki Medina Munguía</h1>
 		</div>
 	)
