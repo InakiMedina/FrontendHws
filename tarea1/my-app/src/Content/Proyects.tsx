cea3350096cb411c98d74a80e113c6cfeb94a409
InakiMedina <inaki.medina@gmail.com>
2026-09-04 21:22:59 -0600

    styled lists

--- tarea1/my-app/src/Content/Proyects.tsx
+++ tarea1/my-app/src/Content/Proyects.tsx
@@ -1,49 +1,51 @@
+import styles from './Proyects.module.css'
+
 export default function Proyects() {
 	return (
 		<>
 			<h3>Main Proyects</h3>
 			<h4>E-Commerce Microservices Architecture & Cloud Infrastructure</h4>
-			<li>
-				<ul>
+			<ul>
+				<li className = {styles.list}>
 					GitHub Organization.
-				</ul>
-				<ul>
+				</li>
+				<li>
 					Architected a microservices backend in python deployed fully using Terraform on AWS and automated CI/CD pipelines.
-				</ul>
-				<ul>
+				</li>
+				<li>
 					Configured networking VPN gateways and Internet Gateways.
-				</ul>
-				<ul>
+				</li>
+				<li>
 					Added metrics and logs with CloudWatch
-				</ul>
-			</li>
+				</li>
+			</ul>
 			<h4>Prolingo – Language Learning Platform</h4>
-			<li>
-				<ul>
+			<ul>
+				<li>
 					Live Deployment.
-				</ul>
-				<ul>
+				</li>
+				<li>
 					Engineered a decoupled, full-stack web application utilizing TypeScript, Node.js/Express, and Vite.
-				</ul>
-				<ul>
+				</li>
+				<li>
 					Designed and implemented a NoSQL document schema using MongoDB
-				</ul>
-			</li>
+				</li>
+			</ul>
 			<h4>HTML Lexical Analyzer & Parser</h4>
-			<li>
-				<ul>
+			<ul>
+				<li>
 					Developed a lexical analyzer and parser for HTML syntax using Python's PLY framework.
-				</ul>
-				<ul>
+				</li>
+				<li>
 					Implemented tokenization rules, regular expressions, and focus on tag matching for document validation and error detection.
-				</ul>
-			</li>
+				</li>
+			</ul>
 			<h4>Low-Level Bitmapped Snake Game</h4>
-			<li>
-				<ul>
+			<ul>
+				<li>
 					Implemented the classic Snake game directly in Assembly Language with direct video memory addressing and bitwise manipulation.
-				</ul>
-			</li>
+				</li>
+			</ul>
 		</>
 	)
 }
